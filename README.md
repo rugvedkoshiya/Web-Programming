73c059b51326e6ffafe8e496ac6578bbaa62e238
Rugved Koshiya <rugvedkoshiya2002@gmail.com>
2022-02-16 17:41:29 +0530

    pract 1 to 6

---
+++ README.md
@@ -0,0 +1,33 @@
+# Web Programming Practicals
+
+## Practical 1
+- Make a web page with Use all HTML tags
+- [See Practical 1](./prac1/)
+
+## Practical 2
+- Design your class timetable using HTML and display it in tabular format 
+- [See Practical 2](./prac2/)
+
+## Practical 3
+- Create a registration form 
+- [See Practical 3](./prac3/)
+
+## Practical 4
+- Demonstrate use of input elements
+- [See Practical 4](./prac4/)
+
+## Practical 5 - 1
+- Demonstrate use of inline and external CSS
+- [See Practical 5 - 1](./prac5_1/)
+
+## Practical 5 - 2
+- Create a fancy table as shown below
+- [See Practical 5 - 2](./prac5_2/)
+
+## Practical 5 - 3
+- Design a personal resume using diffrent properties of CSS and HTML
+- [See Practical 5 - 3](./prac5_3/)
+
+## Practical 6 - 1
+- Write javascript to valudate the following fields of the above registrtion page. 
+- [See Practical 6 - 1](./prac6_1/)
---
+++ prac6_1/script.js
@@ -0,0 +1,57 @@
+function validateForm() {
+    const name = document.getElementById("name");
+    const password = document.getElementById("password");
+    const mobile = document.getElementById("mobile");
+    const validationError = document.getElementById("validationError");
+
+    console.log(password.value);
+    console.log(mobile.value);
+    console.log(validationError);
+
+    nameBool = validateName(name.value);
+    passwordBool = validatePassword(password.value);
+    mobileBool = validateMobile(mobile.value);
+
+    if (!nameBool) {
+        validationError.style.display = "block";
+        validationError.innerText = "Error: Name should be atleast 5 character long"
+        return false;
+    } else if (!passwordBool) {
+        validationError.style.display = "block";
+        validationError.innerText = "Error: Password should be atleast 8 character long"
+        return false;
+    } else if (!mobileBool) {
+        validationError.style.display = "block";
+        validationError.innerText = "Error: Mobile number should be 10 digit long"
+        return false;
+    } else {
+        validationError.style.display = "none";
+        return true;
+    }
+
+
+}
+
+function validatePassword(password) {
+    if (password.length >= 8) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateMobile(mobile) {
+    if (mobile.length == 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateName(name) {
+    if (name.length >= 5) {
+        return true;
+    } else {
+        return false;
+    }
+}
